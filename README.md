2acd93f88adb5536cc7975fc58c6e2e256b880c9
reasonly8 <reasonly8@sina.com>
2024-11-28 17:04:38 +0800

    format

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Recursively read the contents of a folder（递归读取文件夹中的内容）
 
-See []()
+See [./src/index.ts](./src/index.ts)
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { loadProject } from './utils/loadProject';
 import * as path from 'node:path';
 
 const filesMap = await loadProject(path.resolve(__dirname, '..'));
+
 console.log(Object.keys(filesMap))
-// writeFileSync('test.txt', JSON.stringify(filesMap, null, 2));
+
