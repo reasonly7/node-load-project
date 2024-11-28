148a63e15261e26577c719d6b01a1bc276c7efd5
reasonly8 <reasonly8@sina.com>
2024-11-28 17:48:49 +0800

    chore: add nodemon --ignore output/

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import { loadProject } from './utils/loadProject';
 import * as path from 'node:path';
+import * as fs from 'node:fs/promises';
 
 const filesMap = await loadProject(path.resolve(__dirname, '..'));
 
-console.log(Object.keys(filesMap))
+const prefixPath = path.resolve(__dirname, '../output');
+const dirCheckMap: Record<string, boolean> = {};
 
+for (const [fileRelativePath, content] of Object.entries(filesMap)) {
+  const fileFullPath = path.resolve(prefixPath, fileRelativePath);
+  const fileDir = path.dirname(fileFullPath);
+
+  if (!dirCheckMap[fileDir]) {
+    await fs.mkdir(fileDir, { recursive: true });
+    dirCheckMap[fileDir] = true;
+  }
+  
+  fs.writeFile(fileFullPath, content, 'utf-8');
+}
